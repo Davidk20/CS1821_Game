e8dcb0b357951db62136c5f16d9b2b66829720c2
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-24 15:21:30 +0000

    Movement class uses position vector

--- movement.py
+++ movement.py
@@ -3,22 +3,32 @@ try:
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
+from vector import Vector
+
 class Movement:
-  def __init__(self, speed):
+  def __init__(self, speed, pos_vector):
     self.speed = speed
 
   def move_horizontal(self, direction):
+    """
+    Direction: -1 = move left
+    Direction: 1 = move right
+    """
     if direction < 0:
-      #move left
+      self.pos_vector.add(Vector(-speed, 0)) 
       pass
     elif direction > 0:
-      #move right
+      self.pos_vector.add(Vector(speed, 0)) 
       pass
 
   def move_vertical(self, direction):
+    """
+    Direction: -1 = move down
+    Direction: 1 = move up
+    """
     if direction > 0:
-      #move up
+      self.pos_vector.add(Vector(0, speed)) 
       pass
     elif direction < 0:
-      #move down
+      self.pos_vector.add(Vector(0, -speed))
       pass
