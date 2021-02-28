5a54c27138e5eda22794c38edf4ad67d68d1c973
David Kidd <32614126+Davidk20@users.noreply.github.com>
2021-02-28 18:38:00 +0000

    add/remove functions added and player.py commented

--- game.py
+++ game.py
@@ -11,7 +11,11 @@ class Game:
         self.player = Player(30, 350)
         self.kbd = Keyboard()
         self.interaction = Interaction(self.player, self.kbd)
+        self.game_window_setup()
 
+
+    #Setup of SimpleGUI window
+    def game_window_setup(self):
         frame = simplegui.create_frame("Game", 700 , 700, 0)
         frame.set_canvas_background("#534a32")
         frame.set_keydown_handler(self.kbd.keyDown)
@@ -19,10 +23,13 @@ class Game:
         frame.set_draw_handler(self.draw)
         frame.start()
 
+    #Function handling drawing of all shapes on screen
     def draw(self, canvas):
         self.player.draw(canvas)
         self.interaction.update()
 
+
+# Class to link player to keyboard for interactions with other shapes
 class Interaction:
     def __init__(self, player, keyboard):
         self.player = player
