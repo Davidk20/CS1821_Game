ae77e7daa3b92197d07f9223f93037578b338840
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-03-02 18:27:58 +0000

    Convert tabs to spaces"

--- source/keyboard.py
+++ source/keyboard.py
@@ -4,30 +4,30 @@ except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 class Keyboard:
-  def __init__(self):
-      self.left = False
-      self.right = False
-      self.up = False
-      self. down = False
+	def __init__(self):
+		self.left = False
+		self.right = False
+		self.up = False
+		self.down = False
 
-  def keyDown(self, key):
-      if key == simplegui.KEY_MAP['a'] or key == simplegui.KEY_MAP['left']:
-        self.left = True
-      elif key == simplegui.KEY_MAP['d'] or key == simplegui.KEY_MAP['right']:
-        self.right = True
-      elif key == simplegui.KEY_MAP['w'] or key == simplegui.KEY_MAP['up']:
-        self.up = True
-      elif key == simplegui.KEY_MAP['s'] or key == simplegui.KEY_MAP['down']:
-        self.down = True
+	def keyDown(self, key):
+		if key == simplegui.KEY_MAP['a'] or key == simplegui.KEY_MAP['left']:
+			self.left = True
+		elif key == simplegui.KEY_MAP['d'] or key == simplegui.KEY_MAP['right']:
+			self.right = True
+		elif key == simplegui.KEY_MAP['w'] or key == simplegui.KEY_MAP['up']:
+			self.up = True
+		elif key == simplegui.KEY_MAP['s'] or key == simplegui.KEY_MAP['down']:
+			self.down = True
 
-  def keyUp(self, key):
-      if key == simplegui.KEY_MAP['a'] or key == simplegui.KEY_MAP['left']:
-        self.left = False
-      elif key == simplegui.KEY_MAP['d'] or key == simplegui.KEY_MAP['right']:
-        self.right = False
-      elif key == simplegui.KEY_MAP['w'] or key == simplegui.KEY_MAP['up']:
-        self.up = False
-      elif key == simplegui.KEY_MAP['s'] or key == simplegui.KEY_MAP['down']:
-        self.down = False
-    
+	def keyUp(self, key):
+		if key == simplegui.KEY_MAP['a'] or key == simplegui.KEY_MAP['left']:
+			self.left = False
+		elif key == simplegui.KEY_MAP['d'] or key == simplegui.KEY_MAP['right']:
+			self.right = False
+		elif key == simplegui.KEY_MAP['w'] or key == simplegui.KEY_MAP['up']:
+			self.up = False
+		elif key == simplegui.KEY_MAP['s'] or key == simplegui.KEY_MAP['down']:
+			self.down = False
+		
     
