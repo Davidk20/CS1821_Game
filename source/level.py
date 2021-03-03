1bddd9eac3b154e87fcfdae26a8f089d4a87de83
Alex L <75644112+AlexLalov@users.noreply.github.com>
2021-03-03 11:58:49 +0200

    The 2d grid map implementation
    There are already 3 maps that I've made I will soon enough try to implement a system which infinitely makes them from presets.

---
+++ source/level.py
@@ -0,0 +1,104 @@
+try:
+    import simplegui
+except ImportError :
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+WIDTH = 720
+HEIGHT = 720
+
+WALL = 1
+
+LEVEL_GRID_CENTRE = [
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+    [ 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+    [ 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+    [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [ 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+    [ 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+]
+
+LEVEL_GRID_1 = [
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+    [ 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+    [ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [ 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    [ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+    [ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+]
+
+LEVEL_GRID_2 = [
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+    [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [ 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [ 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+]
+
+
+
+class Level:
+    def __init__(self, grid):
+        self.grid = grid
+        self.grid_width = len(grid[0])
+        self.grid_height = len(grid)
+        self.cell_width = WIDTH // self.grid_width
+        self.cell_height = HEIGHT // self.grid_height
+
+    def draw(self, canvas):
+        for y in range(self.grid_height):
+            for x in range (self.grid_width):
+                self.draw_cell(x, y, canvas)
+
+    def draw_cell (self, x, y, canvas):
+        if self.is_wall(x, y):
+
+            start_x = x * self.cell_width
+            start_y = y * self.cell_height
+
+            canvas.draw_polygon([[start_x, start_y], [start_x + self.cell_width, start_y], [start_x + self.cell_width, start_y + self.cell_height], [start_x, start_y + self.cell_height]], 0, 'Yellow', 'Orange')
+
+    def is_wall(self, x, y):
+        if x < 0:
+            return False
+        if x >= self.grid_width:
+            return False
+        if y < 0:
+            return False
+        if y >= self.grid_height:
+            return False
+        return self.grid[y][x] == WALL
+
+class Interaction:
+    def __init__(self, level):
+        self.level = level
+    def draw(self, canvas):
+        self.level.draw(canvas)
+
+
+level_start = Level(LEVEL_GRID_CENTRE)
+level_1 = Level(LEVEL_GRID_1)
+level_2 = Level(LEVEL_GRID_2)
+interaction = Interaction(level_2)
+
+frame = simplegui.create_frame("My Level", WIDTH, HEIGHT)
+frame.set_draw_handler(interaction.draw)
+frame.start()
