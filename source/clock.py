fc30b1e7cc0920859f076573ab8f505b10c15726
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-03-15 16:09:41 +0000

    Improve clock class and implement into game

--- source/clock.py
+++ source/clock.py
@@ -1,12 +1,13 @@
 class Clock:
-  def __init__(self):
-    self.time = 0
+  time = 0
 
-  def tick(self):
-    self.time += 1
+  @classmethod
+  def tick(cls):
+    cls.time += 1
 
-  def transition(self, frame_duration):
-    if self.time % frame_duration == 0:
+  @classmethod
+  def transition(cls, frame_duration):
+    if cls.time % frame_duration == 0:
       return True
     else:
       return False
